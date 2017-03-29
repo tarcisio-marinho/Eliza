6d908fe7cbdf20f9b4a9b33f59d2ddcb18d62b21
Tarcísio Marinho <tarcisio_marinho09@hotmail.com>
2017-03-28 21:35:56 -0300

    Add files via upload

---
+++ musica.py
@@ -0,0 +1,25 @@
+#!/bin/bash/env python
+# coding=UTF-8
+# by Tarcisio marinho
+# github.com/tarcisio-marinho
+import os
+from colorama import Fore
+
+
+def play(data):
+    if len(data[5:]) == 0:
+        print(Fore.BLUE + "Song name doesn't exist. (music '"'song name'"') " + Fore.RESET)
+
+    else:
+        wanted = data[6:]
+        find = os.popen("ls | grep -i " +'"'+ wanted +'"')
+        music = str(find.readline())
+
+        if not music:
+            os.system("instantmusic -s " + wanted)
+            find = os.popen("ls -tc --hide='__*' --hide='*.py'")
+            music = str(find.readline()).replace("\n", "")
+            os.system("XDG_CURRENT_DESKTOP= DESKTOP_SESSION= xdg-open " + music.replace(" ", "\ ").replace(" (", " \("). replace(")", "\)"))
+
+        else:
+            os.system("XDG_CURRENT_DESKTOP= DESKTOP_SESSION= xdg-open " + music.replace(" ", "\ ").replace(" (", " \("). replace(")", "\)"))
