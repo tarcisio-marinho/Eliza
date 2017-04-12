89a36a554fff8cbe165beb9a33671c5118058faa
tarcisio-marinho <tarcisio_marinho09@hotmail.com>
2017-04-12 14:46:43 -0300

    update musica.py

--- modulos/musica.py
+++ modulos/musica.py
@@ -19,8 +19,8 @@ def musica():
         caminho=os.environ['HOME'] +'/'+ pasta+'/'
         resp=os.path.isdir(caminho)
         if(resp==True): # existe
-            #arquivo_config=open('.config.txt','a')
-            #arquivo_config.write(caminho)
+            arquivo_config=open('.config.txt','a')
+            arquivo_config.write(caminho)
             print('Existe')
             os.chdir(caminho)
             arquivos=os.listdir(os.getcwd())
@@ -71,7 +71,51 @@ def musica():
         else:
             print('Não existe')
 
-    else:
-        print('eXiste')
+    else: # ja esta salvo e ele so escolhe a musica
+        arquivo_config=open('.config.txt','r')
+        caminho=arquivo_config.readline()
+        os.chdir(caminho)
+        arquivos=os.listdir(os.getcwd())
+        tam=len(arquivos)
+        while(i<tam):
+            dicionario[i]=arquivos[i]
+            if(os.path.isfile(arquivos[i])):
+                print(i,'File->', dicionario[i])
+            elif(os.path.isdir(arquivos[i])):
+                print(i,'Dir ->', dicionario[i])
+            i=i+1
+
+        escolha=raw_input('Escolha: ')
+        escolha=int(escolha)
+        while(escolha<0 or escolha>len(dicionario)-1): # SE DIGITAR STRING ELE BUGA
+            print('Numero fora da lista')
+            escolha=input('Escolha: ')
+            escolha=int(escolha)
+        os.system('clear')
+        if(os.path.isfile(dicionario[escolha])): # arquivo - tocar musica
+            a=1
+        elif(os.path.isdir(dicionario[escolha])): # diretorio - escolher musica primeiro para depois tocar
+            dicionario2={}
+            os.chdir(os.getcwd()+'/'+dicionario[escolha])
+            musicas=os.listdir(os.getcwd())
+            tam=len(musicas)
+            i=0
+
+            while(i<tam):
+                dicionario2[i]=musicas[i]
+                if(os.path.isfile(musicas[i])):
+                    print(i,'File->', dicionario2[i])
+                elif(os.path.isdir(musicas[i])):
+                    print(i,'Dir ->', dicionario2[i])
+                i=i+1
+            escolha=raw_input('Escolha: ')
+            escolha=int(escolha)
+            while(escolha<0 or escolha>len(dicionario2)-1): # SE DIGITAR STRING ELE BUGA
+                print('Numero fora da lista')
+                escolha=input('Escolha: ')
+                escolha=int(escolha)
+            print(dicionario2[escolha])
+            musica=dicionario2[escolha].replace(" ", "\ ").replace(" (", " \("). replace(")", "\)")
+            os.system('xdg-open '+musica)
 
 musica()
