6c5cd5db6866e181364186007fd757de0850f484
Tarcísio Marinho <tarcisio_marinho09@hotmail.com>
2017-04-24 01:57:16 -0300

    Update cliente.py

--- modulos/ssh/cliente.py
+++ modulos/ssh/cliente.py
@@ -1,31 +1,33 @@
-#!/usr/bin/env python
-# by Tarcisio Marinho
+#!/bin/bash/env python
+# coding=UTF-8
+# by Tarcisio marinho
 # github.com/tarcisio-marinho
 from socket import *
 from RSA.criptografa import *
 
-serverHost='192.168.15.107'
-porta=6062
+def conecta():
+    serverHost='192.168.15.107'
+    porta=6062
 
-socket_obj = socket(AF_INET, SOCK_STREAM)
-socket_obj.connect((serverHost, porta))
+    socket_obj = socket(AF_INET, SOCK_STREAM)
+    try:
+        socket_obj.connect((serverHost, porta))
+    except socket.error:
+        print('Servidor offline\n')
 
-# chave publica
-data = socket_obj.recv(1024) # recebeu do servidor a chave publica
-chave_publica=data.split(',') # separou a chave -> N e E
-print(chave_publica)
+    # chave publica
+    data = socket_obj.recv(1024) # recebeu do servidor a chave publica
+    chave_publica=data.split(',') # separou a chave -> N e E
+    print('Conectado ao servidor\nConexão criptografada\n')
 
-while True:
+    while True:
 
-    frase=raw_input('->') # texto a ser criptografado e enviado
-    criptografado=cipher(frase,int(chave_publica[0]),int(chave_publica[1])) # criptografou o texto
-    print(criptografado)
-    string=str(criptografado)
-    string=string.replace('[',' ').replace(']',' ').replace(' ','')
-    mensagem=b'%s' %(string) # enviou para o servidor em forma de string o texto
-    socket_obj.send(mensagem)
+        frase=raw_input('Comandos -$ ') # texto a ser criptografado e enviado
+        criptografado=cipher(frase,int(chave_publica[0]),int(chave_publica[1])) # criptografou o texto
+        string=str(criptografado)
+        string=string.replace('[',' ').replace(']',' ').replace(' ','')
+        mensagem=b'%s' %(string) # enviou para o servidor em forma de string o texto
+        socket_obj.send(mensagem)
 
 
-    #a = raw_input('Voce: ')
-    #mensagem = b'%s' % (a)
-    #socket_obj.send(mensagem)
+conecta()
