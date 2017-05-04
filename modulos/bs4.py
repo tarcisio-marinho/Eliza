e3947b517998a6a50902eee925f5f1238077d38f
Tarcísio Marinho <tarcisio_marinho09@hotmail.com>
2017-05-04 10:44:41 -0300

    Update bs4.py

--- modulos/bs4.py
+++ modulos/bs4.py
@@ -3,19 +3,19 @@ import lxml
 import requests
 
 
-def busca(string)
-texto_html = requests.GET('https://www.google.com.br'+string)
+def busca(string):
+    texto_html = requests.GET('https://www.google.com.br'+string)
 
-bs_obj = bs.BeautifulSoup(texto_html.text(), 'lxml') # continua em html porém como objeto bs4
+    bs_obj = bs.BeautifulSoup(texto_html.text(), 'lxml') # continua em html porÃ©m como objeto bs4
 
-#bs_obj.title.string titulo 
-# bs_obj.find_all('p') ## google -> div data-hveid
+    #bs_obj.title.string titulo 
+    # bs_obj.find_all('p') ## google -> div data-hveid
 
-for paragraph in soup.find_all('p'): # texto na tag
-    print(paragraph.text)
+    for paragraph in soup.find_all('p'): # texto na tag
+        print(paragraph.text)
 
-for url in bs_obj.find_all('a'): # links
-    print(url.get('href'))
+    for url in bs_obj.find_all('a'): # links
+        print(url.get('href'))
 
-for div in soup.find_all('div', class_='g'):
-    print(div.text)
+    for div in soup.find_all('div', class_='g'):
+        print(div.text)
