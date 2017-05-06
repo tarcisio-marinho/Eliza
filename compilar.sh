0fa9e4037f020b9929dc31922a6710c64f6b722d
Tarcísio Marinho <tarcisio_marinho09@hotmail.com>
2017-05-06 14:33:14 -0300

    Update compilar.sh

--- compilar.sh
+++ compilar.sh
@@ -5,4 +5,4 @@ sudo pip install requests
 sudo pip install lxml
 sudo pip install beautifulsoup4
 sudo pip install SpeechRecognition
-sudo apt-get install python-pyaudio python3-pyaudio
+sudo apt-get install portaudio19-dev python-all-dev python3-all-dev && sudo pip install pyaudio
