f8d3afefe8a1ed6c7a4a31de7f406391563c1064
Tarcísio Marinho <tarcisio_marinho09@hotmail.com>
2017-05-06 14:29:18 -0300

    Update compilar.sh

--- compilar.sh
+++ compilar.sh
@@ -4,3 +4,5 @@ sudo apt-get install python-pip
 sudo pip install requests
 sudo pip install lxml
 sudo pip install beautifulsoup4
+sudo pip install SpeechRecognition
+sudo apt-get install python-pyaudio python3-pyaudio
