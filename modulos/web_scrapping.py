0fe977b8654bb52bc29c3e7649ea03bcc24b91a8
marinho <tarcisio_marinho09@hotmail.com>
2017-05-06 18:48:18 -0300

    origin

--- modulos/web_scrapping.py
+++ modulos/web_scrapping.py
@@ -5,26 +5,41 @@
 import bs4 as bs
 import lxml
 import requests
+import os
 
 
-def busca(string):
-    texto_html = requests.GET('https://www.google.com.br'+string)
+def busca(mecanismo,string):
+    if(mecanismo=='wikipedia' or mecanismo=='wiki'):
+        try:
+            texto_html = requests.get('https://pt.wikipedia.org/wiki/'+string)
+            bs_obj = bs.BeautifulSoup(texto_html.text, 'lxml') # continua em html porém como objeto bs4
+        except requests.exceptions.ConnectionError:
+            print('Sem conexão a internet')
+            return
 
-    bs_obj = bs.BeautifulSoup(texto_html.text(), 'lxml') # continua em html porém como objeto bs4
+        try:
+            if(str(bs_obj.p.text[0])+ str(bs_obj.p.text[1])+str(bs_obj.p.text[2])=='A W'):
+                print('nao achou')
+                return
+        except:
+            pass
 
-    #bs_obj.title.string titulo
-    # bs_obj.find_all('p') ## google -> div data-hveid
+        i=0
+        for paragraph in bs_obj.find_all('p'):
+            i=i+1
+            print(paragraph.text)
+            if(i==3):
+                break
 
-    for paragraph in soup.find_all('p'): # texto na tag
-        print(paragraph.text)
+    elif(mecanismo=='google'):
+        texto_html = requests.get('https://www.google.com.br/#q='+string)
+        bs_obj = bs.BeautifulSoup(texto_html.text, 'lxml')
+        for div in bs_obj.find_all('a'):
+            print(div.get('data-href'))
 
-    for url in bs_obj.find_all('a'): # links
-        print(url.get('href'))
-
-    for div in soup.find_all('div', class_='g'):
-        print(div.text)
-
-
+while True:
+    pesquisa=raw_input('busca: ')
+    busca('google',pesquisa)
 # YOUTUBE
 #class_="yt-lockup-content"
 #<a.title>
